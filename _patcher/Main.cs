95c60ec4d5c992316de543b8982fea0d5ed45f9a
nigga/nigger <anoletenu@gmail.com>
2026-03-01 23:09:34 +0700

    Remove unused check

--- _patcher/Main.cs
+++ _patcher/Main.cs
@@ -14,24 +14,6 @@ namespace _patcher
         {
             try
             {
-#if DEBUG
-                // server checks so they don't get banned on bancho
-                // NOTE: on debug since this already being handled on https://github.com/refx-online/patcher-cli
-                var args = Environment.GetCommandLineArgs();
-                if (!args.Contains("-devserver") || string.IsNullOrEmpty(args.SkipWhile(arg => arg != "-devserver")
-                    .Skip(1)
-                    .FirstOrDefault()) ||
-                    args.SkipWhile(arg => arg != "-devserver")
-                        .Skip(1)
-                        .First()
-                        .Contains("ppy.sh"))
-                {
-                    // can just use Environment.Exit(-1); but this is better
-                    GameBase.BeginExit();
-                    return 1;
-                }
-#endif
-
                 // now patchall
                 Harmony.PatchAll(typeof(Main).Assembly);
 
